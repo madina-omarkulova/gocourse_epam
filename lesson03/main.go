22d9d458c460833629d4030a0953418bd9825e10
madina-omarkulova <74660149+madina-omarkulova@users.noreply.github.com>
2021-07-13 21:58:31 +0600

    hw3

---
+++ lesson03/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func averv(a [6]int) float64 {
+	s := 0.0
+	for _, k := range a {
+		s += float64(k)
+	}
+	return s / 6
+}
+
+func max(s []string) string {
+	ans := s[0]
+	mx := len(s[0])
+	for _, k := range s {
+		if len(k) > mx {
+			ans = k
+			mx = len(k)
+		}
+	}
+	return ans
+}
+
+func reverse(k []int64) []int64 {
+	var ans []int64
+	for i := len(k) - 1; i > -1; i-- {
+		ans = append(ans, k[i])
+	}
+	return ans
+}
+
+func printSorted(m map[int]string) {
+	n := make([]int, 0, len(m))
+	for k := range m {
+		n = append(n, k)
+	}
+	fmt.Print("[")
+	sort.Ints(n)
+	for i, l := range n {
+		fmt.Print("\"", m[l], "\"")
+		if i != len(n)-1 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Println("]")
+}
